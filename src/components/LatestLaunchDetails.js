687a10699efa993313cf6514a39e837eeefb446a
Matt Mills <mattmills1023@gmail.com>
2019-01-13 08:13:23 -0600

    Updating latest launch.

--- src/components/LatestLaunchDetails.js
+++ src/components/LatestLaunchDetails.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { StaticQuery, graphql } from 'gatsby';
+import { StaticQuery, graphql, Link } from 'gatsby';
 import moment from 'moment';
 
 const Wrapper = styled.div`
@@ -10,6 +10,22 @@ const Wrapper = styled.div`
 const Heading = styled.span`
   font-weight: 600;
   margin-right: 0.5rem;
+`;
+
+const MissionName = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-bottom: 0.5rem;
+
+  h1 {
+    line-height: 1;
+  }
+
+  span {
+    color: #27d872;
+    font-size: 0.875rem;
+  }
 `;
 
 const LeftBlock = styled.div`
@@ -30,7 +46,36 @@ const RightBlock = styled.div`
 const Detail = styled.div`
   font-size: 0.875rem;
   &:not(:last-child) {
-    margin-bottom: 0.5rem;
+    margin-bottom: 0.35rem;
+  }
+`;
+
+const Actions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 0.5rem;
+
+  a {
+    padding: 0.75rem 1.75rem;
+    border: 2px solid #ff006b;
+    text-decoration: none;
+    font-weight: 600;
+    transition: all 0.125s ease-in-out;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const Number = styled.div`
+  font-size: 2.25rem;
+  line-height: 1;
+
+  span {
+    font-size: 1.5rem;
+    line-height: 1;
+    vertical-align: top;
   }
 `;
 
@@ -46,7 +91,7 @@ const LatestLaunchDetails = () => {
           launch_site,
           flight_number,
           launch_success,
-          detail,
+          details,
           links,
         } = edges[0].node;
 
@@ -58,8 +103,11 @@ const LatestLaunchDetails = () => {
               <img src={links.mission_patch} alt={`${mission_name} patch`} />
             </LeftBlock>
             <RightBlock>
-              <h1>{mission_name}</h1>
-              <Detail>{detail}</Detail>
+              <MissionName>
+                <h1>{mission_name}</h1>
+                <span>{launch_success ? 'SUCCESSFUL' : 'FAILURE'}</span>
+              </MissionName>
+              <Detail>{details}</Detail>
               <Detail>
                 <Heading>LAUNCH DATE:</Heading>
                 {date.format('MM.DD.YYYY')}
@@ -68,6 +116,15 @@ const LatestLaunchDetails = () => {
                 <Heading>LAUNCH SITE:</Heading>
                 {launch_site.site_name_long}
               </Detail>
+              <Actions>
+                <Link to="/" arial-label="Go to flight details">
+                  LAUNCH DETAILS
+                </Link>
+                <Number>
+                  <span>#</span>
+                  {flight_number}
+                </Number>
+              </Actions>
             </RightBlock>
           </Wrapper>
         );
