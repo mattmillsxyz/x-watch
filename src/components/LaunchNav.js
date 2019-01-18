beac881c5cf503798a7e09e214e7c85df432b07e
Matt Mills <mattmills1023@gmail.com>
2019-01-18 13:34:50 -0600

    Update launch nav.

--- src/components/LaunchNav.js
+++ src/components/LaunchNav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, StaticQuery } from 'gatsby';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -13,23 +13,72 @@ const Wrapper = styled.div`
   }
 `;
 
-const LaunchNav = ({ flightNumber }) => {
-  return (
-    <Wrapper>
+const renderPrevious = (flightNumber, pages) => {
+  // TODO: Make this better
+  let pageArr = [];
+  pages.forEach(page => {
+    if (page.node.flight_number != null) {
+      pageArr.push(page.node.flight_number);
+    }
+  });
+
+  if (pageArr.includes(flightNumber - 1)) {
+    return (
       <Link
         to={`/launch/${flightNumber - 1}`}
         aria-label={`Go to flight number ${flightNumber - 1} details`}
       >
         &laquo; PREVIOUS
       </Link>
+    );
+  }
+  return <div />;
+};
+
+const renderNext = (flightNumber, pages) => {
+  // TODO: Make this better
+  let pageArr = [];
+  pages.forEach(page => {
+    if (page.node.flight_number != null) {
+      pageArr.push(page.node.flight_number);
+    }
+  });
+
+  if (pageArr.includes(flightNumber + 1)) {
+    return (
       <Link
         to={`/launch/${flightNumber + 1}`}
         aria-label={`Go to flight number ${flightNumber + 1} details`}
       >
         NEXT &raquo;
       </Link>
-    </Wrapper>
-  );
+    );
+  }
+  return <div />;
 };
+
+const LaunchNav = ({ flightNumber }) => (
+  <StaticQuery
+    query={allPages}
+    render={data => (
+      <Wrapper>
+        {renderPrevious(flightNumber, data.allInternalPastLaunches.edges)}
+        {renderNext(flightNumber, data.allInternalPastLaunches.edges)}
+      </Wrapper>
+    )}
+  />
+);
+
+const allPages = graphql`
+  {
+    allInternalPastLaunches {
+      edges {
+        node {
+          flight_number
+        }
+      }
+    }
+  }
+`;
 
 export default LaunchNav;
