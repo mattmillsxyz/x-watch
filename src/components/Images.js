fff8a1bad27de249bd5649e2b3663463cccb9782
Matt Mills <mattmills1023@gmail.com>
2019-01-17 23:17:35 -0600

    Lazy load flickr images.

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { LazyImage } from 'react-lazy-images';
+import Spinner from 'react-spinkit';
 
 const Wrapper = styled.div`
   width: 100%;
@@ -21,28 +23,97 @@ const Heading = styled.h5`
 const Container = styled.div`
   padding: 2rem 5% 3rem;
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   flex-wrap: wrap;
+`;
+
+const ImageLink = styled.a`
+  width: calc(25% - 2px);
+
+  @media (max-width: 750px) {
+    width: calc(100% - 2px);
+  }
+`;
+
+const PlaceholderDiv = styled.div`
+  width: 100%;
+  height: 170px;
+  border: 1px solid white;
+  border-bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImageDiv = styled.div`
+  width: 100%;
+  height: 170px;
+  border: 1px solid white;
+  border-bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &.animated {
+    -webkit-animation-duration: 0.75s;
+    animation-duration: 0.75s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+  }
+
+  @-webkit-keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+
+    to {
+      opacity: 1;
+    }
+  }
+
+  &.fadeIn {
+    -webkit-animation-name: fadeIn;
+    animation-name: fadeIn;
+  }
 `;
 
 const renderImages = imageData => {
   return imageData.map((image, index) => {
     return (
-      <a
+      <ImageLink
         href={image}
         key={`flight-image--${index}`}
         aria-label={`View full size launch image number ${index}`}
       >
-        <div
-          style={{
-            background: `url(${image}) center center`,
-            backgroundSize: 'cover',
-            width: '170px',
-            height: '130px',
-            marginBottom: '1.5rem',
-          }}
+        <LazyImage
+          src={image}
+          debounceDurationMs={400}
+          placeholder={({ ref }) => (
+            <PlaceholderDiv ref={ref}>
+              <Spinner name="pulse" />
+            </PlaceholderDiv>
+          )}
+          actual={({ imageProps }) => (
+            <ImageDiv
+              style={{
+                background: `url(${imageProps.src}) center center`,
+                backgroundSize: 'cover',
+              }}
+              className="animated fadeIn"
+            />
+          )}
         />
-      </a>
+      </ImageLink>
     );
   });
 };
