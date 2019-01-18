c5552f2c53d1c556689cae5dbf1d19cd8bdb72e3
Matt Mills <mattmills1023@gmail.com>
2019-01-18 16:46:25 -0600

    Add sticky launch nav.

--- src/components/LaunchNav.js
+++ src/components/LaunchNav.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Link, graphql, StaticQuery } from 'gatsby';
 import styled from 'styled-components';
+import Sticky from 'react-stickynode';
 
 const Wrapper = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
-  margin-bottom: 1rem;
+  background-color: white;
+  padding: 1rem 0;
 
   a {
     font-weight: 600;
   }
 `;
 
-const renderPrevious = (flightNumber, pages) => {
+const renderNavLink = (direction, flightNumber, pages) => {
   // TODO: Make this better
+  const toLaunch = direction === 'prev' ? flightNumber - 1 : flightNumber + 1;
   let pageArr = [];
   pages.forEach(page => {
     if (page.node.flight_number != null) {
@@ -22,35 +25,17 @@ const renderPrevious = (flightNumber, pages) => {
     }
   });
 
-  if (pageArr.includes(flightNumber - 1)) {
+  if (pageArr.includes(toLaunch)) {
     return (
       <Link
-        to={`/launch/${flightNumber - 1}`}
-        aria-label={`Go to flight number ${flightNumber - 1} details`}
+        to={`/launch/${toLaunch}`}
+        aria-label={`Go to flight number ${toLaunch} details`}
       >
-        &laquo; PREVIOUS
-      </Link>
-    );
-  }
-  return <div />;
-};
-
-const renderNext = (flightNumber, pages) => {
-  // TODO: Make this better
-  let pageArr = [];
-  pages.forEach(page => {
-    if (page.node.flight_number != null) {
-      pageArr.push(page.node.flight_number);
-    }
-  });
-
-  if (pageArr.includes(flightNumber + 1)) {
-    return (
-      <Link
-        to={`/launch/${flightNumber + 1}`}
-        aria-label={`Go to flight number ${flightNumber + 1} details`}
-      >
-        NEXT &raquo;
+        {direction === 'prev' ? (
+          <span>&laquo; PREVIOUS</span>
+        ) : (
+          <span>NEXT &raquo;</span>
+        )}
       </Link>
     );
   }
@@ -61,10 +46,20 @@ const LaunchNav = ({ flightNumber }) => (
   <StaticQuery
     query={allPages}
     render={data => (
-      <Wrapper>
-        {renderPrevious(flightNumber, data.allInternalPastLaunches.edges)}
-        {renderNext(flightNumber, data.allInternalPastLaunches.edges)}
-      </Wrapper>
+      <Sticky>
+        <Wrapper>
+          {renderNavLink(
+            'prev',
+            flightNumber,
+            data.allInternalPastLaunches.edges
+          )}
+          {renderNavLink(
+            'next',
+            flightNumber,
+            data.allInternalPastLaunches.edges
+          )}
+        </Wrapper>
+      </Sticky>
     )}
   />
 );
