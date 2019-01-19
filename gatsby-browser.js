dc66cfa3e7c330cefbe0423eb4eba617993aa8dc
Matt Mills <mattmills1023@gmail.com>
2019-01-19 15:11:54 -0600

    Fix Safari issue.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+exports.onClientEntry = () => {
+  // IntersectionObserver polyfill for gatsby-image (Safari, IE)
+  if (typeof window.IntersectionObserver === `undefined`) {
+    import(`intersection-observer`);
+    console.log(`👍 IntersectionObserver is polyfilled`);
+  }
+};
