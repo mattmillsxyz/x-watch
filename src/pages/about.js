686b4970d53e828d158748ded74bccc583274762
Matt Mills <mattmills1023@gmail.com>
2019-01-19 11:59:01 -0600

    Update text.

--- src/pages/about.js
+++ src/pages/about.js
@@ -67,15 +67,15 @@ const AboutPage = () => {
       <Container>
         <Story>
           <p>
-            Yeah, this website is dedicated. To all the teachers that told me
+            {`Yeah, this website is dedicated. To all the teachers that told me
             I'd never amount to nothin'. To all the people that lived above the
             buildings that I was codin' in front of. Called the police on me
             when I was just tryin' to make some money to feed my daughter (it's
             all good). And all the homies in the struggle. You know what I'm
-            sayin'? It's all good, baby baby.
+            sayin'? It's all good, baby baby.`}
           </p>
           <p>
-            It was all a dream, I used to read Air & Space magazine. Elon Musk
+            {`It was all a dream, I used to read Air & Space magazine. Elon Musk
             and Jim Cantrell up in the limousine. Hangin' pictures on my wall.
             Every Saturday Falcon 9, Falcon Heavy, Dragon. I let my engine rock
             'til my engine popped. Smokin' pads in Vandenberg, sippin' on rocket
@@ -87,15 +87,15 @@ const AboutPage = () => {
             used to eat sardines for dinner. Peace to Alan Shep, Neil A, Big
             Buzz. Flightmaster Griss, Cap'n Conrad. I'm blowin' up like you
             thought I would. Call the crib, same number, same hood. It's all
-            good (It's all good). And if you don't know, now you know, Jeffrey.
+            good (It's all good). And if you don't know, now you know, Jeffrey.`}
           </p>
           <p>
-            You know very well. Who you are. Don't let 'em hold you down. Reach
+            {`You know very well. Who you are. Don't let 'em hold you down. Reach
             for the stars. You had a goal. But not that many. 'Cause you're the
-            only one. I'll give you good and plenty.
+            only one. I'll give you good and plenty.`}
           </p>
           <p>
-            I made the change from a common kid. To up close and personal with
+            {`I made the change from a common kid. To up close and personal with
             Robin Leach. And I'm far from cheap. I pop rockets with my peeps all
             day. Spread love, it's the Austin way. The coffee and RedBull keep
             me wizzy. Girls used to diss me. Now they in my DMs 'cause they miss
@@ -107,15 +107,15 @@ const AboutPage = () => {
             Lunches, brunches, interviews by the pool. Considered a fool 'cause
             I dropped out of grad school. Stereotypes of a white male
             misunderstood. And it's still all good. And if you don't know, now
-            you know, Larry.
+            you know, Larry.`}
           </p>
           <p>
-            You know very well. Who you are. Don't let 'em hold you down. Reach
+            {`You know very well. Who you are. Don't let 'em hold you down. Reach
             for the stars. You had a goal. But not that many. 'Cause you're the
-            only one. I'll give you good and plenty.
+            only one. I'll give you good and plenty.`}
           </p>
           <p>
-            Super Nintendo, Sega Genesis. When I was dead broke, man, I couldn't
+            {`Super Nintendo, Sega Genesis. When I was dead broke, man, I couldn't
             picture this. 50-inch screen, money-green leather sofa. Got two
             rides, an Uber with a driver. Phone bill about two G's flat. No need
             to worry, my accountant handles that. And my whole crew is loungin'.
@@ -126,28 +126,28 @@ const AboutPage = () => {
             heat, wonder why Christmas missed us. Birthdays was the worst days.
             Now we sip Topo when we thirsty. Uh, damn right, I like the life I
             live. 'Cause I went from negative to positive. And it's all (It's
-            all good, Richy B). And if you don't know, now you know, Charlie B.
+            all good, Richy B). And if you don't know, now you know, Charlie B.`}
           </p>
           <p>
-            You know very well. Who you are. Don't let 'em hold you down. And if
+            {`You know very well. Who you are. Don't let 'em hold you down. And if
             you don't know, now you know, Serg. Reach for the stars. You had a
             goal. But not that many. 'Cause you're the only one. And if you
-            don't know, now you know, Kimbal. I'll give you good and plenty.
+            don't know, now you know, Kimbal. I'll give you good and plenty.`}
           </p>
           <p>
-            Representin' A-Town in the house. XYZ Mafia, mad flavor. Uh, uh,
-            yeah, aight.
+            {`Representin' A-Town in the house. XYZ Mafia, mad flavor. Uh, uh,
+            yeah, aight.`}
           </p>
           <p>
-            You know very well. Who you are. Don't let 'em hold you down. Reach
+            {`You know very well. Who you are. Don't let 'em hold you down. Reach
             for the stars. You had a goal. But not that many. 'Cause you're the
-            only one. I'll give you good and plenty.
+            only one. I'll give you good and plenty.`}
           </p>
           <p>
-            Matty M, it's all good, homie. Elon, it's all good, homie. Stevie J,
+            {`Matty M, it's all good, homie. Elon, it's all good, homie. Stevie J,
             it's all good, homie. It's all good. That's right, '19. And on and
             on, and on and on. You know very well. Who you are. Don't let 'em
-            hold you down. Reach for the stars.
+            hold you down. Reach for the stars.`}
           </p>
         </Story>
         <DetailsWrapper>
