e494fd18d054543761996b09253877250ac91639
Matt Mills <mattmills1023@gmail.com>
2019-01-20 21:53:07 -0600

    First attempt at getting all launch photos...

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,16 +2,57 @@ const { each } = require(`lodash`);
 const Promise = require(`bluebird`);
 const path = require(`path`);
 const slash = require(`slash`);
+const { isEmpty } = require(`lodash`);
+const { createRemoteFileNode } = require(`gatsby-source-filesystem`);
 
-exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions;
+exports.onCreateNode = async ({
+  node,
+  actions,
+  store,
+  cache,
+  createNodeId,
+}) => {
+  const { createNodeField, createNode } = actions;
   const { type } = node.internal;
+
   if (type === 'internal__pastLaunches' || type === 'internal__latestLaunch') {
     createNodeField({
       node,
       name: `slug`,
       value: `/launch/${node.flight_number}`,
     });
+  }
+
+  if (type === 'internal__pastLaunches') {
+    if (node.links && !isEmpty(node.links.flickr_images)) {
+      await createNodeField({
+        node,
+        name: `flickr_images`,
+        value: {},
+      });
+
+      node.links.flickr_images.forEach(async (image, index) => {
+        let fileNode;
+
+        try {
+          fileNode = await createRemoteFileNode({
+            url: node.links.flickr_images[index],
+            store,
+            cache,
+            createNode,
+            createNodeId,
+          });
+        } catch (e) {
+          console.error('Error downloading Flickr images:', e);
+        }
+
+        // Adds a field `localImage` or custom name to the node
+        // ___NODE appendix tells Gatsby that this field will link to another node
+        if (fileNode) {
+          node.fields.flickr_images[`image_${index}___NODE`] = fileNode.id;
+        }
+      });
+    }
   }
 };
 
